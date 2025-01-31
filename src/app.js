a905bc202d69b5f9a470e54b2d462b19c05ea6a9
abssailesh <abssailesh@gmail.com>
2025-01-31 14:33:23 +0530

    created a sever

---
+++ src/app.js
@@ -0,0 +1,28 @@
+// console.log("Starting a new Project");
+
+
+const express = require('express');
+// creating a new application of express js
+
+const app = express();
+
+//  Request Handler which is basically how the server responds to the incoming request.
+// app.use((request, response)=>{
+//     response.send("Hello World from the server");
+// })
+app.use("/test",(req,res)=>{
+    res.send("Hello World from the test server");
+    
+});
+app.use("/",(req,res)=>{
+    res.send("Hello World from the server");
+    
+});
+app.use("/hello",(req,res)=>{
+    res.send("Hello World from the hello server");
+});
+// App.listen have a call back function that will only be called when our server is up and running.
+app.listen(3000,()=>{
+    console.log("Server is listening on port 3000");
+    
+}); 
