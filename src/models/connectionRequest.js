409f7e2ffce9a70cd0e6497f2edea69336e600d4
abssailesh <abssailesh@gmail.com>
2025-02-04 21:46:23 +0530

    modified code

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -1,30 +1,30 @@
-const mongoose = require('mongoose');
-const connectionRequestSchema = new mongoose.Schema({
-    fromUserId : {
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    },
-    toUserId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true
-    },  
-    status:{
-        type : String,
-        required:true,
-        enum :{
-            values : ["ignored", "interested", "accepted", "rejected"],
-            message : `{VALUE} is not a valid status`,
-        }
-    }
+const mongoose = require("mongoose");
 
-},
-    {
-        timestamps: true,
-    }
+const connectionRequestSchema = new mongoose.Schema(
+  {
+    fromUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    toUserId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: {
+        values: ["ignored", "interested", "accepted", "rejected"],
+        message: `{VALUE} is incorrect status type`,
+      },
+    },
+  },
+  { timestamps: true }
 );
 
+// ConnectionRequest.find({fromUserId: 273478465864786587, toUserId: 273478465864786587})
 
 connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
